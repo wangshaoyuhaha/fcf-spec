b3fe3c3b6e168eac23d744b75ac91cfe1d70fe25
shaoyu wang <wangshaoyuhaha@gmail.com>
2026-07-05 17:22:07 +0800

    add UI-APP-D2 AI-CONTEXT handoff loader

--- sidecars/ui_app_1/__init__.py
+++ sidecars/ui_app_1/__init__.py
@@ -1,5 +1,12 @@
 """UI-APP-1 sidecar package."""
 
+from .ai_context_handoff_loader import (
+    AI_CONTEXT_HANDOFF_REQUIRED_FIELDS,
+    UIAppHandoffLoadError,
+    load_ai_context_handoff_payload,
+    summarize_ai_context_handoff_payload,
+    validate_ai_context_handoff_payload,
+)
 from .read_only_contract import (
     UI_APP_READ_ONLY_CONTRACT,
     get_read_only_ui_contract,
@@ -7,7 +14,12 @@ from .read_only_contract import (
 )
 
 __all__ = [
+    "AI_CONTEXT_HANDOFF_REQUIRED_FIELDS",
+    "UIAppHandoffLoadError",
     "UI_APP_READ_ONLY_CONTRACT",
     "get_read_only_ui_contract",
+    "load_ai_context_handoff_payload",
+    "summarize_ai_context_handoff_payload",
+    "validate_ai_context_handoff_payload",
     "validate_read_only_ui_contract",
 ]
