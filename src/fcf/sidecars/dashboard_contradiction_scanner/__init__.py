e9b5a2cde97a50d9c2fcf9bd9ff40a5c64636ffa
shaoyu wang <wangshaoyuhaha@gmail.com>
2026-07-10 18:10:18 +0800

    add DASHBOARD-CONTRADICTION-SCANNER-D3 finding schema

--- src/fcf/sidecars/dashboard_contradiction_scanner/__init__.py
+++ src/fcf/sidecars/dashboard_contradiction_scanner/__init__.py
@@ -1,6 +1,12 @@
 """Dashboard contradiction scanner sidecar."""
 
 from .contract import build_contract, validate_contract
+from .finding_schema import (
+    CONTRADICTION_SEVERITIES,
+    FINDING_STATUSES,
+    build_contradiction_finding,
+    validate_contradiction_finding,
+)
 from .source_loader import (
     build_source_manifest,
     load_source_record,
@@ -11,4 +17,8 @@ __all__ = [
     "validate_contract",
     "build_source_manifest",
     "load_source_record",
+    "CONTRADICTION_SEVERITIES",
+    "FINDING_STATUSES",
+    "build_contradiction_finding",
+    "validate_contradiction_finding",
 ]
